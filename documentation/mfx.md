4b92600901acbde24267d51b626296d3537b5be7
suen <matieu.suen@ville-ge.ch>
2019-01-21 17:02:45 +0100

    Adding shunt register

--- documentation/mfx.md
+++ documentation/mfx.md
@@ -8,9 +8,11 @@ The low level implementation can be found under `Documentation/Driver/BSP/Compon
 There is 2 interface USART and I2C. The discovery board is only connected to the I2C by default.
 There is also a MFX_WAKEUP line so we would need a GPIOPin.
 
+I2C run at 100Khz. This value is taken from the stm32cubel4 example.
+
 # Register description
 
-## CTRL: Control register
+## CTRL: Control register 0x80
 
 offset bit:
 
@@ -27,3 +29,18 @@ offset bit:
   * REQ: Start a request.
 
 ## PRE_DELAY:
+
+## SHUNT REGISTER:
+
+There is 5 shunt register in other to configure the ohm for each one of them.
+  
+   * SHUNT0_MSB  0x82
+   * SHUNT0_LSB  0x83
+   * SHUNT1_MSB  0x84
+   * SHUNT1_LSB  0x85
+   * SHUNT2_MSB  0x86
+   * SHUNT2_LSB  0x87
+   * SHUNT3_MSB  0x88
+   * SHUNT3_LSB  0x89
+   * SHUNT4_MSB  0x8A
+   * SHUNT4_LSB  0x8B
--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,23 @@ use hal::digital::OutputPin;
 pub enum Register {
     // Idd control register (R/W)
     CTRL = 0x80,
+    // Read the number of shunt being used in the last idd read
     SHUNT_USED = 0x1A,
 
+    // Shunt resistor configuration
+    // Lets do one incremental write
+    SHUNT0 = 0x82, // MSB 0x83
+    SHUNT1 = 0x84, // MSB 0x85,
+    SHUNT2 = 0x86, // MSB 0x87,
+    SHUNT3 = 0x88, // MSB 0x89,
+    SHUNT4 = 0x8A, // MSB 0x8B,
+
+    // Shunt stabilization in millisecond
+    SH0_STABILIZATION = 0x90,
+    SH1_STABILIZATION = 0x91,
+    SH2_STABILIZATION = 0x92,
+    SH3_STABILIZATION = 0x93,
+    SH4_STABILIZATION = 0x94,
 }
 
 impl Register {
@@ -56,6 +71,31 @@ where
         Ok(())
     }
 
+    pub fn config_shunt0(&mut self, data: u16, stabDelay: u8 ) -> Result<(), E> {
+        self.config_shunt(Register::SHUNT0, data, Register::SH0_STABILIZATION, stabDelay)
+    }
+
+    pub fn config_shunt1(&mut self, data: u16, stabDelay: u8) -> Result<(), E> {
+        self.config_shunt(Register::SHUNT1, data, Register::SH1_STABILIZATION, stabDelay)
+    }
+
+    pub fn config_shunt2(&mut self, data: u16, stabDelay: u8) -> Result<(), E> {
+        self.config_shunt(Register::SHUNT2, data, Register::SH2_STABILIZATION, stabDelay)
+    }
+
+    pub fn config_shunt3(&mut self, data: u16, stabDelay: u8) -> Result<(), E> {
+        self.config_shunt(Register::SHUNT3, data, Register::SH3_STABILIZATION, stabDelay)
+    }
+
+    pub fn config_shunt4(&mut self, data: u16, stabDelay: u8) -> Result<(), E> {
+        self.config_shunt(Register::SHUNT4, data, Register::SH4_STABILIZATION, stabDelay)
+    }
+
+    fn config_shunt(&mut self, regShunt: Register, data: u16, regDelay: Register, stabDelay: u8) -> Result<(), E> {
+        self.i2c.write(self.address, &[regShunt.addr(), (data >> 8) as u8, (data & 0xFF) as u8])?;
+        self.i2c.write(self.address, &[regDelay.addr(), stabDelay])
+    }
+
     pub fn last_shunt_used(&mut self) -> Result<u8, E> {
         let mut buffer: [u8; 1] = unsafe { mem::uninitialized() };
         self.i2c.write_read(self.address, &[Register::SHUNT_USED.addr()], &mut buffer)?;
@@ -63,9 +103,6 @@ where
     }
 
     fn write_register(&mut self, reg: Register, data: u8) -> Result<(), E> {
-        self.i2c.write(
-            self.address,
-            &[reg.addr(), data],
-        )
+        self.i2c.write(self.address, &[reg.addr(), data])
     }
 }
